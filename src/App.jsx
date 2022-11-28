48f3199f1da2fc6e364137de7705d8b3d8212387
lukinoo <luka.bliadze.2015@mail.ru>
2022-11-29 00:41:26 +0400

    core/project-structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello, world</h2>
+      {/* create num counter */}
+    </div>
+  );
+}
+
+export default App;
