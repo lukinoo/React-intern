15056d8e6dc968a340c389c0315b4dc14a294259
lukinoo <luka.bliadze.2015@mail.ru>
2022-11-29 12:44:32 +0400

    adding components folder in the projectand modifying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h2 className="title">hello, world</h2>
-      <p className="foo">ბარემ ჩემიც</p>
-      <div>hello, React</div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
