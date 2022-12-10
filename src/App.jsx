1f056c9d18f1b21bf06c222f9c8d0b3fae0d8a42
etherbits <nika.qvrivishviliPC@gmail.com>
2022-12-10 21:55:31 +0400

    adding error-group

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
+import { useState } from "react";
 import "./App.css";
 import { Dropdown } from "./components/Dropdown";
-
-const words = [
-  "monitor",
-  "program",
-  "application",
-  "keyboard",
-  "javascript",
-  "typescript",
-  "gaming",
-  "network",
-];
+import { ErrorGroup } from "./components/ErrorGroup";
+import { motion } from "framer-motion";
+// const words = [
+//   "monitor",
+//   "program",
+//   "application",
+//   "keyboard",
+//   "javascript",
+//   "typescript",
+//   "gaming",
+//   "network",
+// ];
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null)
+  const [errors, setErrors] = useState({
+    firstname: { message: "", code: null },
+  });
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${username}`)
+            .then(async (res) => {
+                return [res, await res.json()]
+            })
+            .then(([res, data]) => {
+              if(res.ok){
+                return setUser(data)
+              }
+
+              const tempObj = { ...errors };
+              tempObj.firstname = { message: data.message, code: res.status };
+              setErrors(tempObj);
+            })
+  };
+
   return (
     <div className="App">
-      <h2>Header</h2>
-      <Dropdown words={words} />
+      <form
+        style={{ display: "flex", flexDirection: "column", width: "200px" }}
+      >
+        <h2>Header</h2>
+        <ErrorGroup
+          value={username}
+          onChange={handleChange}
+          errors={errors.firstname}
+        />
+        <motion.button layout onClick={handleSubmit}>
+          submit
+        </motion.button>
+         <span>
+         {user ? user.name : 'no user'}
+        </span>
+      </form>
+      {/* <Dropdown words={words} /> */}
     </div>
   );
 };
